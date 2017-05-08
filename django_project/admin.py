61bdd11b3a284aa37dbaf769b92c9eb6862bf67a
Suraj Joshi <surajkincity@gmail.comit config --global user.email surajkincity@gmail.com>
2017-05-08 13:42:14 +0530

    Asd

--- django_project/admin.py
+++ django_project/admin.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import leads,newsletter,contact
+from .models import career,newsletter,contact
 
 
-admin.site.register(leads)
+admin.site.register(career)
 admin.site.register(newsletter)
 admin.site.register(contact)
 
