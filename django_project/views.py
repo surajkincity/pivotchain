67eb06cb6197b9bdbc6fa846c6ad487b12a88b39
Suraj Joshi <surajkincity@gmail.comit config --global user.email surajkincity@gmail.com>
2017-05-11 16:01:19 +0530

    asdasd

--- django_project/views.py
+++ django_project/views.py
@@ -26,7 +26,7 @@ def home(request):
       if form.is_valid():
        post = form.save(commit=False)
        post.save() 
-       return HttpResponseRedirect("https://www.pivotchain.com/#careerform" , { 'display' :display , 'displaya' :displaya , 'message' :message ,'message1' :message1  })
+       return HttpResponseRedirect("https://www.pivotchain.com/#contactlink" , { 'display' :display , 'displaya' :displaya , 'message' :message ,'message1' :message1  })
           
     if "resume" in request.POST:
       message = '<h3 class="thanks">Thanks! We will get back to you shortly!</h3>'
@@ -37,7 +37,7 @@ def home(request):
         display = 'block'
         post = form.save(commit=False)
         post.save()
-        return HttpResponseRedirect("https://www.pivotchain.com/#contactlink" , { 'display' :display , 'displaya' :displaya , 'message' :message ,'message1' :message1  })
+        return HttpResponseRedirect("https://www.pivotchain.com/#careerform" , { 'display' :display , 'displaya' :displaya , 'message' :message ,'message1' :message1  })
       else:
         return render(request, 'about.html', )
           #return render_to_response( 'home.html', { 'display' :display , 'displaya' :displaya , 'message' :message ,'message1' :message1  })    
